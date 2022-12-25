0ff2cf502defd1280e56442730816c9ff02f5309
Kirill Korolev <kkorolev@gmail.com>
2022-12-26 00:24:18 +0300

    fix: leap year check

--- age-calc.js
+++ age-calc.js
@@ -16,7 +16,7 @@
     
     if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
         daysInMonth = 31; // 31 days if month is Jan, Mar, May, Jul, Aug, Oct, Dec
-    } else if (month === 2 && leapYear > 0) {
+    } else if (month === 2 && leapYear !== 0) {
         daysInMonth = 28 // 28 days if month is Feb
     } else if (month === 2 && leapYear === 0) {
         daysInMonth = 29 // 28+1 days if month is Feb and leap year
