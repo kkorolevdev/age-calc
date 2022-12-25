105adee4a75213cda05bf5d251b9b8ec37331f35
Kirill Korolev <kkorolev@gmail.com>
2022-12-26 00:10:11 +0300

    feat: number of days in a given month calculations

--- age-calc.js
+++ age-calc.js
@@ -1,1 +1,20 @@
 //
+const userDate = 15;
+const userMonth = 2;
+const userYear = 1996;
+
+console.log(userDate);
+
+const month = userMonth;
+
+if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+    daysInMonth = 31; // 31 days if month is Jan, Mar, May, Jul, Aug, Oct, Dec
+} else if (month === 2 && userYear !== 1996) {
+    daysInMonth = 28 // 28 days if month is Feb
+} else if (month === 2 && userYear === 1996) {
+    daysInMonth = 29 // 28+1 days if month is Feb and leap year
+} else {
+    daysInMonth = 30; // 30 days if month is Apr, Jun, Sep, Nov
+}
+
+console.log(daysInMonth);
